838c1ef14a7ea3e7e9bd983cc342d8be3385aa43
unknown <hannohilbig@gmail.com>
2018-11-27 19:50:20 -0500

    add stuff

--- 03_Code/01_Municipalities.R
+++ 03_Code/01_Municipalities.R
@@ -16,6 +16,7 @@ library(hannostuff2)
 library(stringr)
 library(readr)
 library(readxl)
+library(openxlsx)
 
 ## ## ## ##
 
@@ -201,4 +202,163 @@ muni17 <- muni17 %>% filter(!is.na(gem)) %>%
          lat = str_replace_all(string = lat, 
                                pattern = ',', 
                                replacement = '\\.')) %>%
-  mutate(lon = as.numeric(lon), lat = as.numeric(lat))
+  mutate(lon = as.numeric(lon), 
+         lat = as.numeric(lat))
+
+## Make a list of the municipalities ## 
+
+list_muni <- list(muni09, muni10, muni11, muni12, muni13,
+                  muni14, muni15, muni16, muni17)
+
+
+
+#### Unemployment data #### 
+
+dir_contents <- dir('01_Raw_Data/Unemployment/')
+
+files_unem_csv <- dir_contents[str_detect(string = dir_contents,
+                                          pattern = '.csv')]
+
+## Load 2017
+
+unem17 <- readxl::read_xlsx(path = '01_Raw_Data/Unemployment/Jahreszahlen 2017-Gemeinde.xlsx',
+                            sheet = 6, 
+                            skip = 9, 
+                            col_names = F)
+
+## Set colnames
+
+cnames <- c('ags', 'name', 'unem_tot', 
+            paste0('other', 1:4), 'unem_foreigner',
+            paste0('other', 5:28))
+colnames(unem17) <- cnames
+
+## Clean up
+
+unem17 <- unem17 %>% filter(nchar(ags) == 8) %>%
+  select(-one_of(paste0('other', 1:28))) %>% 
+  select(ags, unem_tot, unem_foreigner)
+
+## Load 2016
+
+unem16 <- readxl::read_xlsx(path = '01_Raw_Data/Unemployment/2016JZ-gem21_d_0.xlsx',
+                            sheet = 6, 
+                            skip = 9, 
+                            col_names = F)
+
+## Set colnames
+
+cnames <- c('ags', 'name', 'unem_tot', 
+            paste0('other', 1:4), 'unem_foreigner',
+            paste0('other', 5:28))
+colnames(unem16) <- cnames
+
+## Clean up
+
+unem16 <- unem16 %>% filter(nchar(ags) == 8) %>%
+  select(-one_of(paste0('other', 1:28))) %>% 
+  select(ags, unem_tot, unem_foreigner)
+
+## Make this a function
+
+cleanup_unem <- function(filepath) { 
+  df <- read.csv(file = filepath, 
+                 header = F, 
+                 stringsAsFactors = F) %>% 
+    slice(10:n())
+  
+  ## Replace all commas with full stops
+  
+  df[, 3:32] <- apply(df[, 3:32], 2, function(x) {
+    as.numeric(str_replace_all(x, ',', ''))
+  })
+  
+  ## Set colnames
+  
+  cnames <- c('ags', 'name', 'unem_tot', 
+              paste0('other', 1:4), 'unem_foreigner',
+              paste0('other', 5:28))
+  colnames(df) <- cnames
+  
+  ## Clean up
+  
+  df <- df %>% filter(nchar(ags) == 8) %>%
+    select(-one_of(paste0('other', 1:28))) %>% 
+    select(ags, unem_tot, unem_foreigner)
+  
+  ## Return DF
+  
+  df
+}
+
+## Run the function on all filepaths
+
+fpaths <- paste0("01_Raw_Data/Unemployment/", files_unem_csv)
+
+## Function:
+
+list_dfs <- pblapply(fpaths, cleanup_unem)
+names(list_dfs) <- 2009:2015
+
+## Add the last two years to the list
+
+list_dfs[['2016']] <- unem16
+list_dfs[['2017']] <- unem17
+
+## Quick check: Are AGS unique?
+
+cat('This has to be TRUE for all years')
+sapply(list_dfs, function(x) sum(duplicated(x$ags)) == 0)
+
+## Looks good, ready for merging 
+
+out_final_list <- pblapply(1:9, function(i) {
+  df <- left_join(list_muni[[i]], list_dfs[[i]])
+  
+  ## Get unem / capita
+  
+  df <- df %>% mutate(unem_capita = unem_tot / pop_tot)
+  
+  ## Return this
+  
+  df
+}) 
+
+## Check max of unem / capita per year (sanity check)
+
+cat('Max unemployment per capita (THESE SHOULD BE < 1): ',
+    round(sapply(out_final_list, function(x) max(x$unem_capita, na.rm = T)), 3))
+
+## Check the means 
+
+cat('Mean unemployment per capita (THESE SHOULD BE similar): ',
+    round(sapply(out_final_list, function(x) mean(x$unem_capita, na.rm = T)), 3))
+
+## Check the SDs 
+
+cat('SD of  unemployment per capita (THESE SHOULD BE similar): ',
+    round(sapply(out_final_list, function(x) sd(x$unem_capita, na.rm = T)), 3))
+
+## Looks ok I guess
+
+#### Save the data #### 
+
+## List of years
+
+years <- 2009:2017
+
+## ## ## Save this
+
+for (j in 1:length(years)) {
+  
+  ## Write
+  
+  write.csv(out_final_list[[j]], 
+            file = paste0('02_Clean_Data/muni_', years[j], '.csv'),
+            row.names = F, 
+            fileEncoding = 'UTF-8')
+  
+}
+
+
+
